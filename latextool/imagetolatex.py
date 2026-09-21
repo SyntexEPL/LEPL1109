a81b5acd047a026b79ed22f7a56eae04d0a2c1dc
Quentin Aubecq <quentin.aubecq@student.uclouvain.be>
2026-09-21 21:11:50 +0200

    Cours2

--- latextool/imagetolatex.py
+++ latextool/imagetolatex.py
@@ -10,23 +10,22 @@ if len(args) != 2:
 prompt = (
     "Convert this equation image to LaTex"
     "Use $$ equations, you can use multiple if you needed"
-    "If you need to align = signs for example, use \begin{align*}, careful as it already acts as $$, you can't put it inside $$"
+    "If you need to align = signs for example, use \\begin{align*}, careful as it already acts as $$, you can't put it inside $$"
     "Don't necessarily follow the style of the image, I prefer something that looks good rather than an exact copy, for example if there are small integral signs, make them big anyways"
-    "For styled letters use \mathcal{} (only when needed of course)"
+    "For styled letters use \\mathcal{} (only when needed of course)"
     "Only output the LaTex code, no explanation, and put it as plain text, no ```latex."
 )
 
-import google.generativeai as genai
+from google import genai
 from PIL import Image
 
+client = None
 with open("latextool/gemini_key.txt") as f:
-    genai.configure(api_key=f.read())
-
-model = genai.GenerativeModel("gemini-2.5-flash")
+    client = genai.Client(api_key=f.read())
 
 img = Image.open(args[1])
 
-response = model.generate_content([
+response = client.models.generate_content(model="gemini-2.5-flash", contents=[
     img,
     prompt
 ])
